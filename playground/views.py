835c7c6036c1613952bd2bec38fb32cfdece14a4
HadisNkn <hadis.nikanfar@gmail.com>
2026-09-10 14:45:02 +0330

    Add comment to say_hello view

--- playground/views.py
+++ playground/views.py
@@ -1,3 +1,18 @@
+# My first view in the playground app
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def say_hello(request):
+    return HttpResponse('Hello World')
+
+
+
+
+
+
 
 # Create your views here.
+# request -> response
+# request handler
+# actions
