168a45dd11fd72bd3a1e3475057496464d4184d9
HadisNkn <hadis.nikanfar@gmail.com>
2026-09-22 23:18:42 +0330

    Debugging in VSCode

--- playground/views.py
+++ playground/views.py
@@ -6,7 +6,13 @@ from django.http import HttpResponse
 def say_hello(request):
     return HttpResponse('Hello World')
 
+def calculate():
+    x = 1
+    y = 2
+    return x
+
 def say_hello_2(request):
+    x = calculate()
     return render(request, 'hello.html', {'name': 'Hadis'})
 
 
